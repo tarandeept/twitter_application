a5a00de33111daa8f147c8ddba30bb045263e119
Taran Thamrait <Taran@tarans-macbook-pro.local>
2019-02-02 16:16:51 -0800

    added login logout features

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Taran Thamrait on 2/2/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    
+}
