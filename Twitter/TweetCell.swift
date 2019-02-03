16d8430ce4696f87314caec4b238a5ed5938e987
Taran Thamrait <Taran@tarans-macbook-pro.local>
2019-02-02 17:17:07 -0800

    tweets now appear correctly

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Taran Thamrait on 2/2/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
