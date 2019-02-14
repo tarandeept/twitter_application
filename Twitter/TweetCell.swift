22446d62aa1239e4925b02bbff696c61b73c3c6a
Taran Thamrait <Taran@Tarans-MacBook-Pro.local>
2019-02-13 21:54:14 -0800

    added favoriting feature

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -13,7 +13,48 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContentLabel: UILabel!
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed \(error)")
+            })
+            
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Could not unfavorite tweet \(error)")
+            })
+        }
+    }
+    
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    
+    
+
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
 
 
     override func awakeFromNib() {
